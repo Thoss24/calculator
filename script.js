adec12e775add2eaa8ff78d9ff48c062351e0097
Thomas Fogarty <t.fogarty24@outlook.com>
2022-08-22 11:07:53 +0100

    add, subtract, multiply, divide functions created

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(addA, addB) {
+    console.log(addA + addB)
+}
+add(2, 2);
+
+function subtract(subA, subB) {
+    console.log(subA - subB)
+}
+subtract(10, 4);
+
+function multiply(mulA, mulB) {
+    console.log(mulA * mulB)
+}
+multiply(4, 4);
+
+function divide(divA, divB) {
+    console.log(divA / divB)
+}
+divide(10, 2)
