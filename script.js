f7126bfd7da0b3c3abfcad14639e97e2af855b53
Thomas Fogarty <t.fogarty24@outlook.com>
2022-08-27 11:52:08 +0100

    operator overwriting previous operator selection bug fixed

--- script.js
+++ script.js
@@ -14,11 +14,6 @@ clearButton.addEventListener('click', removeLast);
 // keyboard support for calculator
 window.addEventListener('keydown', keyboardSupport)
 
-// operator keys events
-let addition = document.getElementById('addition');
-addition.addEventListener('click', add);
-
-
 // functions for basic math operators
 function add(a, b) {
    return a + b
@@ -41,12 +36,17 @@ function divide(a, b) {
 function keyboardSupport(press) {
     if (press.key >= 0 && press.key < 10) printNumber(press.key)
 }
-// keyboardSupport()
+keyboardSupport()
 
 // remove last number entered
 function removeLast() {
     display.textContent = display.textContent.slice(0, -1);
     displayValue = display.textContent
+
+    if (operator && waitingForSecondNumber) {
+        displayValue = '0';
+        display.textContent = '0';
+    }
 }
 removeLast()
 
@@ -56,6 +56,7 @@ function clearScreen() {
     displayValue = '0';
 }
 
+// appends a decimal to screen, only allowing one decimal on the screen at a time
 function inputDecimal(dot) {
     if (!displayValue.includes(dot)) {
         displayValue += dot
@@ -84,23 +85,29 @@ function printNumber(input) {
         displayValue = input;                // displayValue is overwritten by the digit that was input by user
         display.textContent = input;         // and the display textContent will also be overwritten
         waitingForSecondNumber = false       // and waiting for second number will be reset back to false.
-    }                                        // When the user presses an operator waitingForSecondNumber will be set to true and the display will be reset awaiting new input.
+    }                                        // When the user presses an operator waitingForSecondNumber will be set to true and the display will be reset awaiting new input, repeating the process over and over.
      
     console.log(displayValue)
 }
 
-// 
+// function to save chosen operator and input values
 function handleOperator(nextOperator) {
-    const inputVal = parseFloat(displayValue);                   // parseFloat converts string content of displayValue to a number.
+    const inputVal = parseFloat(displayValue);   // parseFloat converts string content of displayValue to a floating point number.
 
-    if (firstNumber === null && !isNaN(inputVal)) {              // check that the firstNumber is equal to null and is not a NaN value.
+    if (operator && waitingForSecondNumber) {    // check if an operator exists and if waitingForSecondNumber is true
+        operator = nextOperator;                 // if true, the value of operator is swapped with value of nextOperator
+        console.log(operator)                    // this enables the user to press a different operator after having already pressed one, and the new operator will be used rather than the old one.
+        return
+    }
+
+    if (firstNumber === null && !isNaN(inputVal)) {              // check that the firstNumber is equal to null and is not a NaN value (basically checking that firstNumber = Number)
         firstNumber = inputVal;                                  // if previous condition is true then update firstNumber to equal inputVal (current display value).
     } else if (operator) {
         const result = operate(firstNumber, inputVal, operator); // check if operator has been assigned an operator. If true operate() function is invoked calculating firstNumber with secondNumber (inputVal) using operator input by user.
                                                                  
         displayValue = String(result);                           // the result of the operate() function calculation is then passed to the display value, screen text content and the firstNumber variable
         display.textContent = String(result);                    // allowing the user to see the result of the calculation, whilst also enabling the result to be used in the next calculation.
-        firstNumber = result                                     // for example; 5 + 5 = 10, 10 is assigned to displayValue, screen content & firstNumber. This value can then be used, for example; 10 + 5 = 15.
+        firstNumber = result                                     // for example; 5 + 5 = 10, 10 is assigned to displayValue, screen content & firstNumber. This value can then be used, for example; display value = 10: + 5 = 15.
     }                                                            // this allows the user to chain calculations together without having to clear all after a single operator is used.
     
     waitingForSecondNumber = true;                               // operator is then set to nextOperator which is equal to whatever operator the user pressed
